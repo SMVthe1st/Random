017cba642bd39d3cdc712e73b90a72c66a62e8d5
S. M. V <117322465+SMVthe1st@users.noreply.github.com>
2023-09-01 10:19:37 -0400

    Create promise-demo.js

---
+++ promise-demo.js
@@ -0,0 +1,13 @@
+const prom = new Promise ((resolve, reject) => {
+    const x = "hi";
+    const y = "heya";
+    x === y ? resolve() : reject();
+})
+
+prom.
+    then (() => {
+        console.log("same phrase");
+    }).
+    catch (() => {
+        console.log("different phrase");
+    });
