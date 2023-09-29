dd88cb487041f6aef0a80c7452da6b0007684845
S. M. V <117322465+SMVthe1st@users.noreply.github.com>
2023-09-29 13:59:19 -0400

    Update sentence-reorder.js

--- sentence-reorder.js
+++ sentence-reorder.js
@@ -1,5 +1,5 @@
 const string = (a) => {
-  return a.split("").sort(() => {return 0.5 - Math.random()}).join("");
+  return a.split("").sort(() => { return 0.5 - Math.random() }).join("");
 };
 
 console.log(string("the quick brown fox jumps over the lazy dog"));
